c4ea1872ed92ffa81771850e3c7ccb78aa84b5ec
Jakub <Jakub@192.168.2.234>
2016-03-26 19:24:29 +0100

    Akce pro export dat na statek.

--- webInfo/src/cz/plsi/webInfo/actions/export/ExportData.java
+++ webInfo/src/cz/plsi/webInfo/actions/export/ExportData.java
@@ -1,12 +1,18 @@
 package cz.plsi.webInfo.actions.export;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Set;
 import java.util.TimeZone;
+import java.util.TreeSet;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -15,6 +21,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import cz.plsi.webInfo.client.TeamStageClient;
 import cz.plsi.webInfo.shared.dataStore.entities.Stage;
 import cz.plsi.webInfo.shared.dataStore.entities.TeamStage;
 import cz.plsi.webInfo.shared.dataStore.entities.TeamStageHelp;
@@ -67,6 +74,62 @@ public class ExportData extends HttpServlet {
 	}
 
 	private enum ExportTypes {
+		TEAM_RESULTS("results.txt") {
+			
+			@Override
+			public StringBuilder getExportedData() {
+				TeamStage teamStage = new TeamStage();
+				List<TeamStage> teamStages = teamStage.getList();
+				teamStage.setStageOrder(TeamStage.TEAM_ENDED_GAME);
+				List<TeamStage> teamsThatEnded = teamStage.getList();
+				HashSet<String> teamEndedNames = new HashSet<String>(15);
+				for (TeamStage teamThatEnded : teamsThatEnded) {
+					teamEndedNames.add(teamThatEnded.getTeamName());
+				}
+				
+				SimpleDateFormat sdf = getSimpleDateFormat();
+				Map<String, TeamStageClient> teamStagesByTeamName = new HashMap<>(36);
+				for (TeamStage actualTeamStage : teamStages) {
+					String actualTeamName = actualTeamStage.getTeamName();
+					try {
+						if (!teamStagesByTeamName.containsKey(actualTeamName)
+								&& ((sdf.parse("2015-09-20 08:00:00").compareTo(actualTeamStage.getStageDate()) >= 0 
+										&& actualTeamStage.getStageOrder() < 14)
+									|| actualTeamStage.getStageOrder() == 14)) {
+							Date date = new Date(actualTeamStage.getStageDate().getTime());
+							TeamStageClient teamStageClient = new TeamStageClient(actualTeamStage.getTeamName(),
+																				actualTeamStage.getStageName(),
+																				actualTeamStage.getStageOrder(),
+																				date);
+							teamStageClient.setEnded(teamEndedNames.contains(teamStageClient.getTeamName()));
+							teamStagesByTeamName.put(actualTeamName,teamStageClient);
+						}
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+				TreeSet<TeamStageClient> sortedTeamStages = new TreeSet<>(Collections.reverseOrder());
+				for (TeamStageClient teamStageClient : teamStagesByTeamName.values()) {
+					sortedTeamStages.add(teamStageClient);
+				}
+				
+				SimpleDateFormat stf = getSimpleTimeFormat();
+				StringBuilder sb = new StringBuilder();
+				sb.append("place;team_name;stageOrder;time\n");
+				int place = 1;
+				for (TeamStageClient teamStageClientForCsv : sortedTeamStages) {
+					sb.append(place++).append(";");
+					sb.append(teamStageClientForCsv.getTeamName()).append(";");
+					sb.append(teamStageClientForCsv.getStageOrder()).append(";");
+					sb.append(stf.format(teamStageClientForCsv.getStageDate()))
+					.append("\n");
+				}
+				return sb;
+			}
+			
+		},
 		TEAM_STAGES("arrivals.txt") {
 
 			@Override
@@ -184,6 +247,13 @@ public class ExportData extends HttpServlet {
 		private static SimpleDateFormat getSimpleDateFormat() {
 			SimpleDateFormat sdf = new SimpleDateFormat(
 					"yyyy-MM-dd HH:mm:ss", new Locale("cs", "CZ"));
+			sdf.setTimeZone(TimeZone.getTimeZone("Europe/Prague"));
+			return sdf;
+		}
+		
+		private static SimpleDateFormat getSimpleTimeFormat() {
+			SimpleDateFormat sdf = new SimpleDateFormat(
+					"HH:mm:ss", new Locale("cs", "CZ"));
 			sdf.setTimeZone(TimeZone.getTimeZone("Europe/Prague"));
 			return sdf;
 		}
